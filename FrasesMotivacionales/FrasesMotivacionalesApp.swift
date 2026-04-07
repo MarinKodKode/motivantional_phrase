c61f62f86ea4f7fe898fd7c25c09ab1610bd4701
LettuuceMx <ahmedlol@outlook.com>
2026-04-07 11:42:59 -0600

    [feat] - se agrega modelos y entidades

--- FrasesMotivacionales/FrasesMotivacionalesApp.swift
+++ FrasesMotivacionales/FrasesMotivacionalesApp.swift
@@ -5,19 +5,28 @@
 //  Created by Manuel Alejandro Hernandez Marín on 06/04/26.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct FrasesMotivacionalesApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Frases.self,
+            Categoria.self,
+            Autor.self,
+            FrasesFavoritas.self,
         ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        let modelConfiguration = ModelConfiguration(
+            schema: schema,
+            isStoredInMemoryOnly: false
+        )
 
         do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+            return try ModelContainer(
+                for: schema,
+                configurations: [modelConfiguration]
+            )
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
---
+++ FrasesMotivacionales/Models/CategoriasModel.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct CategoriasModel{
+    var id:Int
+    var name:String
+}
